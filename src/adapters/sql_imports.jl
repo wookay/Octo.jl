2f255f69aab35578b41298808dd05f59e5da0dc5
WooKyoung Noh <wookay.noh@gmail.com>
2018-03-04 22:57:26 +0900

    IS NULL

--- src/adapters/sql_imports.jl
+++ src/adapters/sql_imports.jl
@@ -6,6 +6,7 @@ import .Octo.AdapterBase: SELECT, DISTINCT, FROM, AS, WHERE, LIKE, EXISTS, AND, 
 import .Octo.AdapterBase: INNER, OUTER, LEFT, RIGHT, FULL, JOIN, ON, USING
 import .Octo.AdapterBase: GROUP, BY, HAVING, ORDER, ASC, DESC
 import .Octo.AdapterBase: CREATE, DROP, DATABASE, TABLE, IF, INSERT, VALUES, UPDATE, SET, DELETE
+import .Octo.AdapterBase: IS, NULL
 
 # import: aggregate functions
 import .Octo.AdapterBase: COUNT, SUM, AVG
