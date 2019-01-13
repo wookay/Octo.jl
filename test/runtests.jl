b22bcdfccd36f038f9cc9713842370d5b6676b1f
WooKyoung Noh <wookay.noh@gmail.com>
2019-01-14 08:30:34 +0900

    ODBC dataframes_test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using Test
+using Jive
 
 ignores = Set()
 
@@ -19,34 +19,4 @@ if startswith(@__FILE__, "/home/jrun/Octo")
     push!(ignores, joinpath("adapters", "jdbc"))
 end
 
-function runtests(tests)
-    n_passed = 0
-    anynonpass = 0
-    for (idx, filepath) in enumerate(all_tests)
-        numbering = string(idx, /, length(all_tests))
-        ts = @testset "$numbering $filepath" begin
-            include(filepath)
-        end
-        n_passed += ts.n_passed
-        anynonpass += ts.anynonpass
-    end
-    if iszero(anynonpass)
-        printstyled("✅   ", color=:green)
-        print("All ")
-        printstyled(n_passed, color=:green)
-        println(" tests have been completed.")
-    end
-end
-
-all_tests = []
-for (root, dirs, files) in walkdir(".")
-    for filename in files
-        !endswith(filename, ".jl") && continue
-        "runtests.jl" == filename && continue
-        filepath = joinpath(root, filename)[3:end]
-        !isempty(ARGS) && !any(x->startswith(filepath, x), ARGS) && continue
-        isempty(ARGS) && any(path->occursin(path, filepath), ignores) && continue
-        push!(all_tests, filepath)
-    end
-end
-runtests(all_tests)
+runtests(@__DIR__, skip=collect(ignores))
