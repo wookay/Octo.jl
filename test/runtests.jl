266f3ec57cd7ef1d5b562838b80cc5f0973e1732
WooKyoung Noh <wookay.noh@gmail.com>
2019-01-23 20:47:49 +0900

    distributed tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,4 +19,9 @@ if startswith(@__FILE__, "/home/jrun/Octo")
     push!(ignores, joinpath("adapters", "jdbc"))
 end
 
-runtests(@__DIR__, skip=collect(ignores))
+using Jive.Distributed: nprocs
+if nprocs() > 1 || !(get(ENV, "JIVE_PROCS", "") in ["", "0"])
+    push!(ignores, joinpath("adapters", "jdbc"))
+end
+
+runtests(@__DIR__, skip=["adapters/mysql/options.jl", ignores...])
