6992c6b35f6f5c19b1d8a16446c3cfeb01032643
WooKyoung Noh <wookay.noh@gmail.com>
2020-02-25 04:05:08 +0900

    ignores test jdbc on Julia v1.1.1

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,8 @@ if Sys.iswindows()
     push!(ignores, joinpath("adapters", "mysql"))
     push!(ignores, joinpath("adapters", "odbc"))
     push!(ignores, joinpath("adapters", "jdbc"))
+elseif VERSION < v"1.3.0"
+    push!(ignores, joinpath("adapters", "jdbc"))
 end
 
 if haskey(ENV, "TRAVIS") && ENV["TRAVIS_OS_NAME"] == "osx"
@@ -19,6 +21,7 @@ using Jive.Distributed: nprocs
 if nprocs() > 1 || !(get(ENV, "JIVE_PROCS", "") in ["", "0"]) || VERSION >= v"1.3.0-alpha"
     push!(ignores, joinpath("adapters", "jdbc"))
 end
+
 
 # LibPQ v0.11.1
 #     ERROR: LoadError: ArgumentError: cannot convert NULL to string
