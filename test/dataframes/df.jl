4b449032e8843c4236200497c148bd214304f4c3
WooKyoung Noh <wookay.noh@gmail.com>
2021-10-16 00:39:05 +0900

    test destructing df

--- test/dataframes/df.jl
+++ test/dataframes/df.jl
@@ -15,3 +15,29 @@ df = DataFrame(result, copycols=false)
 @test result == NamedTuple.(eachrow(df))
 
 end # module test_dataframes_df
+
+
+using Jive
+@If VERSION >= v"1.7" module test_dataframes_df_destructing
+
+using Test
+using DataFrames
+
+result = [(id=1, title="Hello"),
+          (id=2, title="World"),
+         ]
+
+df = DataFrame(result, copycols=false)
+
+row = eachrow(df)[1]
+(; id, title) = row
+@test id == 1
+@test title == "Hello"
+
+titles = []
+for (; title) in eachrow(df)
+    push!(titles, title)
+end
+@test titles == ["Hello", "World"]
+
+end # @If VERSION >= v"1.7" module test_dataframes_df_destructing
